2915fee8f97a40bc241370d4cdaed4ea39b8da38
Ramthor <ramkumar978678@gmail.com>
2024-02-02 12:29:20 +0530

    home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home-container">
+        <nav className="hm__nav">
+          <ul>
+            <li>home</li>
+            <li>home</li>
+            <li>home</li>
+            <li>home</li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Home;
